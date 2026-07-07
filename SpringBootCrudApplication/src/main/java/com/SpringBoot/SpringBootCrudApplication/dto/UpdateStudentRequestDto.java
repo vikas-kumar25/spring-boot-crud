5ae71bd5fc1dda15017a5683415e55ba1dcbcb78
vikas-kumar25 <vikaskumarkxz@gmail.com>
2026-07-08 01:40:55 +0530

    Update student using DTO (excludes email updates)

---
+++ SpringBootCrudApplication/src/main/java/com/SpringBoot/SpringBootCrudApplication/dto/UpdateStudentRequestDto.java
@@ -0,0 +1,8 @@
+package com.SpringBoot.SpringBootCrudApplication.dto;
+
+/**********************************************
+Developer Name: Vikas
+Created on: 08-07-2026 00:24
+Project Name: SpringBootCrudApplication ${
+/**********************************************/public class UpdateStudentRequestDto {
+}
